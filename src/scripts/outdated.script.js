96081c2ed14d3e547252af3d9ce5bbf7207e0950
Or Assayag <orassayag@gmail.com>
2021-05-25 07:29:39 +0300

    clean script

--- src/scripts/outdated.script.js
+++ src/scripts/outdated.script.js
@@ -1,7 +1,8 @@
 const errorScript = require('./error.script');
 require('../services/files/initiate.service').initiate('outdated');
 const OutdatedLogic = require('../logics/outdated.logic');
-
-(async () => {
-    await new OutdatedLogic().run();
-})().catch(e => errorScript.handleScriptError(e, 1));
+try {
+    new OutdatedLogic().run();
+} catch (error) {
+    errorScript.handleScriptError(error, 1);
+}
